69baf68b436255eca71ec63578a2fdef4bc03165
Alexander Kovalev <sanchopanca.alone@gmail.com>
2015-03-24 00:22:53 +0300

    Add function which divide text to paragraphs

--- books.py
+++ books.py
@@ -1,6 +1,15 @@
 import falcon
 
 
+def get_paragraphs(pathname):
+    result = []
+    with open(pathname) as f:
+        for line in f.readlines():
+            if line != '\n':
+                result.append(line[:-1])
+    return result
+
+
 class BooksResource:
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
@@ -12,3 +21,8 @@ app = falcon.API()
 books = BooksResource()
 
 app.add_route('/books', books)
+
+
+if __name__ == '__main__':
+    paragraphs = get_paragraphs('/home/sanchopanca/Documents/thunder.txt')
+    print(paragraphs)
